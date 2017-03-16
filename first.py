d9f0132d7cf197e66534518cdbb232461a8f9083
faseenan <faseena.nabil@gmail.com>
2017-03-16 11:20:56 +0400

    my first programe

---
+++ first.py
@@ -0,0 +1,28 @@
+#Author: Faseena
+#Date: 16/March/2017
+
+
+''' 
+(1) how to take inputs from the user. 
+input is a built in function which always return str 
+(2) learned type conversion str into int and viceversa
+ 
+ if condition
+
+ if (condition == True):
+ 	print (yes)
+else:
+	print (no)
+
+'''
+
+i = input()
+i = int(i)
+print (i)
+print (type(i)) 
+
+if (i > 2):
+	print ('greater than 2 ')
+else:
+	print ('not greater than 2')
+
