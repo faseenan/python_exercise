59646e587096579776f51aac9aa6279a5aaa72c3
Nabil Rahiman <nr83@ADUAE02108LP-MX.local>
2019-04-20 15:46:10 +0400

    removed white lines

--- print_board.py
+++ print_board.py
@@ -1,14 +1,8 @@
-
-
-
 def dispay_board(x='a',ncol=3,nrow=4):
 	s='|'+' '+x+' '
 	line= '*---'*ncol+'*'+'\n'+s*ncol+'|'+'\n'
 	print(line*nrow, end='')
 	print('*---'*ncol+'*')
-
-
-#a=input('Enter the value ')
 
 dispay_board('a',6,9)
 
